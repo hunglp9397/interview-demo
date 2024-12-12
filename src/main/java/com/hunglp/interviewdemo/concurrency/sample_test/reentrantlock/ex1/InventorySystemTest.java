0ec70c840ab8cae8caae43781a8171c4279a28cf
hunglp9397 <lephihung0997@gmail.com>
2024-12-12 22:55:22 +0700

    reentrantlock

---
+++ src/main/java/com/hunglp/interviewdemo/concurrency/sample_test/reentrantlock/ex1/InventorySystemTest.java
@@ -0,0 +1,38 @@
+package com.hunglp.interviewdemo.concurrency.sample_test.reentrantlock.ex1;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class InventorySystemTest {
+    private final ReentrantLock lock = new ReentrantLock();
+
+
+    private int itemInStock = 100;
+
+    public void decrementStock(){
+        lock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "_" + itemInStock);
+            if (itemInStock > 0) {
+                itemInStock--;
+            } else {
+                System.out.println("Out of stock!");
+            }
+        } catch (Exception e) {
+            System.out.println("Error while decrementing item!" + e.getMessage());
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        InventorySystemTest inventorySystemTest = new InventorySystemTest();
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        for(int i = 0; i < 20; i++){
+            executorService.submit( () -> inventorySystemTest.decrementStock());
+        }
+        executorService.shutdown();
+    }
+}
